4db44d4b28b773085191b4c0a2a3e1b224201d7d
Chris DiMaio <chrisdimaio@gmail.com>
2015-12-01 21:17:55 -0500

    First commit.

---
+++ src/test/java/datanormaliztiontool/GenericTableDataTests.java
@@ -0,0 +1,15 @@
+package datanormaliztiontool;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests for GenericTableData class.
+ */
+public class GenericTableDataTests {
+  @Test
+  public void loadDataTest(){
+    
+  }
+}
