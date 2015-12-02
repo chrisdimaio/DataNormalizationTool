85e514a6d284f3f2292554acd25f59bfeb6d86c6
Chris DiMaio <chrisdimaio@gmail.com>
2015-12-01 21:42:27 -0500

    Created load data test.

---
+++ src/test/java/datanormaliztiontool/GenericTableDataExpected.java
@@ -0,0 +1,25 @@
+package datanormaliztiontool;
+
+/**
+ * Created by cdimaio on 12/1/2015.
+ */
+public class GenericTableDataExpected {
+  static final String LOAD_DATA_TEST_ONE =
+          "{0={0=Removed, 1=SCHOOL_YEAR, 2=ORG_CODE, 3=REC_NBR, 4=FIRSTNAME, 5=LASTNAME, 6"
+          + "=MIDDLENAME, 7=DATEOFBIRTH, 8=TOWNCODE, 9=GRADE, 10=DOB_YEAR}, 1={8=001, 9=02, "
+          + "4=James, 5=Derman, 6=A., 7=4/15/08}, 2={8=001, 9=K, 4=Ben, 5=Nedzweski, 6=B., 7"
+          + "=2/10/10}, 3={8=001, 9=K, 4=Bruce, 5=Darosa, 6=C., 7=8/16/2010}, 4={8=001, 9=02"
+          + ", 4=Julie, 5=Menchiari, 6=D., 7=12/14/2007}, 5={8=001, 9=K, 4=Jenni, 5=Askey, 6"
+          + "=E., 7=7/2/2010}, 6={8=001, 9=PK, 4=Timothy, 5=De gruttola, 6=H., 7=4/29/2011},"
+          + " 7={8=001, 9=PK, 4=Sarah, 5=Tilton, 6=A., 7=1/16/2011}, 8={8=001, 9=K, 4=Michel"
+          + "e, 5=Bai, 6=B., 7=12/2/09}, 9={8=001, 9=PK, 4=Carson, 5=Slanda, 6=C., 7=3/25/2"
+          + "011}, 10={8=001, 9=03, 4=Tom, 5=Brenner, 6=D., 7=5/2/2007}, 11={8=001, 9=01, 4"
+          + "=Jessica, 5=Culasso, 6=E., 7=9/1/2009}, 12={8=001, 9=PK, 4=George, 5=Ciacci, 6"
+          + "=F., 7=2/6/2010}, 13={8=001, 9=PK, 4=Chris, 5=Montilio, 6=G., 7=1/20/11}, 14={"
+          + "8=001, 9=01, 4=Cameron, 5=Kipling, 6=H., 7=10/17/2008}, 15={8=001, 9=K, 4=Alex"
+          + ", 5=Musinsky, 6=B., 7=8/31/2010}, 17={8=001, 9=PK, 4=Drake, 5=Mandell, 6=D., 7"
+          + "=6/30/2011}, 16={8=001, 9=02, 4=Andrew, 5=Sebile, 6=C., 7=6/29/2007}, 19={8=00"
+          + "1, 9=01, 4=Jessica, 5=Culasson, 6=E., 7=9/1/2009}, 18={8=001, 9=02, 4=James, 5"
+          + "=Derman, 6=A., 7=4/15/08}, 20={8=001, 9=01, 4=Jack, 5=Nicholson, 6=Johnson, 7="
+          + "9/1/2014}}";
+}
--- src/test/java/datanormaliztiontool/GenericTableDataTests.java
+++ src/test/java/datanormaliztiontool/GenericTableDataTests.java
@@ -1,15 +1,26 @@
 package datanormaliztiontool;
 
+import static org.junit.Assert.assertEquals;
+
+import datanormalizationtool.GenericTableData;
+import datanormalizationtool.TableData;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.io.File;
+
+
 
 /**
  * Tests for GenericTableData class.
  */
 public class GenericTableDataTests {
+  static final String TEST_RESOURCES_DIR = "./build/resources/test/generictabledatatests/";
+
   @Test
-  public void loadDataTest(){
-    
+  public void loadDataTestOne() {
+    String data = TEST_RESOURCES_DIR + "load_data_test_one.xlsx";
+    TableData table = new GenericTableData();
+    table.loadData(new File(data));
+    assertEquals(GenericTableDataExpected.LOAD_DATA_TEST_ONE, table.toString());
   }
 }
