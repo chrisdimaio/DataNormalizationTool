8aebddbb7f3c53c55303a70986d6e0f99dde03d7
Chris <chrisdimaio@gmail.com>
2015-12-02 20:11:40 -0500

    Added new sample data.

--- src/main/java/datanormalizationtool/MainClass.java
+++ src/main/java/datanormalizationtool/MainClass.java
@@ -3,12 +3,13 @@ package datanormalizationtool;
 import java.io.File;
 
 class MainClass {
-  private static final String SAMPLE_SCHOOL_DATA
-      = "./build/resources/main/SampleWaitlist_SCHOOL.xlsx";
+  private static final String RESOURCE_DIR = "./build/resources/main/";
+  private static final String SAMPLE_DESE_DATA = RESOURCE_DIR + "FY16_waitlist_October_TEST.xlsx";
+  private static final String SAMPLE_SCHOOL_DATA = RESOURCE_DIR + "SampleWaitlist_SCHOOL.xlsx";
 
   public static void main(String[] args) {
     TableData table = new GenericTableData();
-    table.loadData(new File(SAMPLE_SCHOOL_DATA));
+    table.loadData(new File(SAMPLE_SCHOOL_DATA), 0);
     System.out.println(table);
   }
 }
