95a2223847651d894647e976369292cc55a312e0
Chris <chrisdimaio@gmail.com>
2015-12-02 20:09:25 -0500

    Added foundDataSheet()

--- src/main/java/datanormalizationtool/GenericTableData.java
+++ src/main/java/datanormalizationtool/GenericTableData.java
@@ -26,48 +26,35 @@ public class GenericTableData extends TableData {
   /**
    * Loads data from file system data into this of instance of GenericTableData.
    * @param file The file to be loaded.
+   * @param sheetIndex the index of the sheet to load data from.
    */
-  public void loadData(final File file) {
-    Workbook workbook;
-    Sheet sheet = null;
+  public void loadData(File file, int sheetIndex) {
+    Workbook workbook = null;
 
     try {
       FileInputStream inputStream = new FileInputStream(file);
       workbook = new XSSFWorkbook(inputStream);
-      sheet = workbook.getSheetAt(0);
     } catch (Exception e) {
       e.printStackTrace();
     }
 
-    if (sheet != null && findHeaderRow(sheet)) {
-      setCell(0, COL_REMOVED, "Removed");
-      setCell(0, COL_SCHOOL_YEAR, "SCHOOL_YEAR");
-      setCell(0, COL_ORD_CODE, "ORG_CODE");
-      setCell(0, COL_REC_NBR, "REC_NBR");
-      setCell(0, COL_FIRSTNAME, "FIRSTNAME");
-      setCell(0, COL_LASTNAME, "LASTNAME");
-      setCell(0, COL_MIDDLENAME, "MIDDLENAME");
-      setCell(0, COL_DATEOFBIRTH, "DATEOFBIRTH");
-      setCell(0, COL_TOWNCODE, "TOWNCODE");
-      setCell(0, COL_GRADE, "GRADE");
-      setCell(0, COL_DOB_YEAR, "DOB_YEAR");
-
+    if (foundDataSheet(workbook)) {
       Row row;
-      for (int r = headerRowIndex + 1; r < sheet.getLastRowNum(); r++) {
-        row = sheet.getRow(r);
-        setCell(r, COL_FIRSTNAME, cellToString(row.getCell(rawFirstNameIndex)));
-        setCell(r, COL_LASTNAME, cellToString(row.getCell(rawLastNameIndex)));
-        setCell(r, COL_MIDDLENAME, cellToString(row.getCell(rawMiddleNameIndex)));
-        setCell(r, COL_DATEOFBIRTH, cellToString(row.getCell(rawBirthDateIndex)));
-        setCell(r, COL_TOWNCODE, cellToString(row.getCell(rawTownCodeIndex)));
-        setCell(r, COL_GRADE, cellToString(row.getCell(rawGradeIndex)));
+      for (int r = headerRowIndex + 1; r < dataSheet.getLastRowNum(); r++) {
+        row = dataSheet.getRow(r);
+        setCell(r, DeseTable.COL_FIRSTNAME, cellToString(row.getCell(rawFirstNameIndex)));
+        setCell(r, DeseTable.COL_LASTNAME, cellToString(row.getCell(rawLastNameIndex)));
+        setCell(r, DeseTable.COL_MIDDLENAME, cellToString(row.getCell(rawMiddleNameIndex)));
+        setCell(r, DeseTable.COL_DATEOFBIRTH, cellToString(row.getCell(rawBirthDateIndex)));
+        setCell(r, DeseTable.COL_TOWNCODE, cellToString(row.getCell(rawTownCodeIndex)));
+        setCell(r, DeseTable.COL_GRADE, cellToString(row.getCell(rawGradeIndex)));
       }
     } else {
       System.out.println("No header row found.");
     }
   }
 
-  private boolean findHeaderRow(Sheet sheet) {
+  protected boolean foundHeaderRow(Sheet sheet) {
     boolean headerRowFound = false;
 
     Iterator<Row> iterator = sheet.iterator();
