bc5373d238c92c6e9a7aab42939c65f900ed0a26
Chris <chrisdimaio@gmail.com>
2015-12-03 21:35:35 -0500

    Updated expected data.

--- src/test/java/datanormaliztiontool/DeseTableExpected.java
+++ src/test/java/datanormaliztiontool/DeseTableExpected.java
@@ -4,5 +4,9 @@ package datanormaliztiontool;
  * Created by cdimaio on 12/2/2015.
  */
 public class DeseTableExpected {
-  public static
+  public static final String DESE_TABLE_INSTANTIATION_TEST = 
+          "{0={0='f:NO_FLAG v:Removed', 1='f:NO_FLAG v:SCHOOL_YEAR', 2='f:NO_FLAG v:ORG_CODE', 3='f"
+          + ":NO_FLAG v:REC_NBR', 4='f:NO_FLAG v:FIRSTNAME', 5='f:NO_FLAG v:LASTNAME', 6='f:NO_FLAG"
+          + " v:MIDDLENAME', 7='f:NO_FLAG v:DATEOFBIRTH', 8='f:NO_FLAG v:TOWNCODE', 9='f:NO_FLAG v:"
+          + "GRADE', 10='f:NO_FLAG v:DOB_YEAR'}}";
 }
