663f13859609514765901c700b6bad3be9f7b0f7
Chris <chrisdimaio@gmail.com>
2015-12-09 23:40:23 -0500

    Scratch work.

--- src/main/java/datanormalizationtool/MainClass.java
+++ src/main/java/datanormalizationtool/MainClass.java
@@ -4,14 +4,15 @@ import java.io.File;
 
 class MainClass {
   private static final String RESOURCE_DIR = "./build/resources/main/";
+  
   private static final String SAMPLE_DESE_DATA = RESOURCE_DIR + "FY16_waitlist_October_TEST.xlsx";
   private static final String SAMPLE_SCHOOL_DATA = RESOURCE_DIR + "SampleWaitlist_SCHOOL.xlsx";
+  private static final String SAMPLE_DUPLICATE_DATA = RESOURCE_DIR + "Duplicates.xlsx";
 
   public static void main(String[] args) {
     TableData table = new GenericTableData();
-    table.loadData(new File(SAMPLE_SCHOOL_DATA), 0);
+    table.loadData(new File(SAMPLE_DUPLICATE_DATA), 0);
     Normalizer normalizer = new Normalizer();
     normalizer.normalize(table);
-    System.out.println("cell 3,5: " + table.getCell(3, 5));
   }
 }
