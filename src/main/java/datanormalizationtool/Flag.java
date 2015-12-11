fd3f811154c9111fb4b01d8142e2b2ecfea71a7e
Chris <chrisdimaio@gmail.com>
2015-12-10 22:46:39 -0500

    Reordered flags.

--- src/main/java/datanormalizationtool/Flag.java
+++ src/main/java/datanormalizationtool/Flag.java
@@ -4,9 +4,9 @@ package datanormalizationtool;
  * Enum of cell flags.
  */
 public enum Flag {
+  AGE_GRADE_MISMATCH,
   NO_FLAG,
   NO_MIDDLE_NAME,
-  AGE_GRADE_MISMATCH,
   UNKNOWN_TOWN,
   UNKNOWN_GRADE,
 }
