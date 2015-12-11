780461e116448677503b45ce77941f5487b12241
Chris DiMaio <chrisdimaio@gmail.com>
2015-12-11 14:34:54 -0500

    Added big sample data.

--- src/main/java/datanormalizationtool/MainClass.java
+++ src/main/java/datanormalizationtool/MainClass.java
@@ -5,14 +5,22 @@ import java.io.File;
 class MainClass {
   private static final String RESOURCE_DIR = "./build/resources/main/";
   
-  private static final String SAMPLE_DESE_DATA = RESOURCE_DIR + "FY16_waitlist_October_TEST.xlsx";
-  private static final String SAMPLE_SCHOOL_DATA = RESOURCE_DIR + "SampleWaitlist_SCHOOL.xlsx";
-  private static final String SAMPLE_DUPLICATE_DATA = RESOURCE_DIR + "Duplicates.xlsx";
+  private static final String SAMPLE_DESE_DATA = "FY16_waitlist_October_TEST.xlsx";
+  private static final String SAMPLE_SCHOOL_DATA = "SampleWaitlist_SCHOOL.xlsx";
+  private static final String SAMPLE_DUPLICATE_DATA = "Duplicates.xlsx";
+  private static final String SAMPLE_BIG_DATA = "Big.xlsx";
 
   public static void main(String[] args) {
     TableData table = new GenericTableData();
-    table.loadData(new File(SAMPLE_DUPLICATE_DATA), 0);
+    table.loadData(new File(RESOURCE_DIR + SAMPLE_DUPLICATE_DATA), 0);
     Normalizer normalizer = new Normalizer();
     normalizer.normalize(table);
+//    System.out.println("Starting...");
+//    long start = System.currentTimeMillis();
+//    normalizer.normalize(table);
+//    long end = System.currentTimeMillis();
+//    System.out.println("Done!");
+//    long delta = (end - start) / 1000;
+//    System.out.println("It took " + delta + " seconds to complete.");
   }
 }
