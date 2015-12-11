73e999074eefda57f3225c1ed8b29643b11b2a1a
Chris <chrisdimaio@gmail.com>
2015-12-10 21:43:06 -0500

    Create formatDate(), calculateAge(), and renamed standardizeDate().

--- src/main/java/datanormalizationtool/DateHandler.java
+++ src/main/java/datanormalizationtool/DateHandler.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.time.DateUtils;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -25,21 +26,52 @@ public class DateHandler {
   };
   
   /**
-   * Takes in dates of various formats and returns them in a standard format.
+   * Takes in dates of various formats and returns a Date instance.
    * @param inputDate date of various format
-   * @return date in standard format.
+   * @return date Date instance.
    */
-  public static Date standardizeDates(String inputDate) {
+  public static Date standardizeDate(String inputDate) {
     inputDate = inputDate.toLowerCase();
     inputDate = inputDate.replaceAll("st|nd|rd|th", "");
     
     try {
       Date date = DateUtils.parseDateStrictly(inputDate, DATE_PATTERNS);
-      DateFormat formatter = new SimpleDateFormat(STANDARD_FORMAT, Locale.US);
-      return formatter.parse(formatter.format(date));
+      return date;
     } catch (Exception e) {
       e.printStackTrace();
     }
     return null;
+  }
+  
+  /**
+   * Returns string representation of date formatted according to STANDARD_FORMAT.
+   * @param date Date instance to format.
+   * @return string representation of date.
+   */
+  public static String formatDate(Date date) {
+    DateFormat formatter = new SimpleDateFormat(STANDARD_FORMAT, Locale.US);
+    return formatter.format(date);
+  }
+  
+  public static int calculateAge(String dobstr) {
+    try {
+      Calendar dob = Calendar.getInstance(Locale.US);
+      Calendar today = Calendar.getInstance(Locale.US);
+      
+      DateFormat formatter = new SimpleDateFormat(STANDARD_FORMAT, Locale.US);
+      dob.setTime(formatter.parse(dobstr));
+      
+      int age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+      if (today.get(Calendar.MONTH) < dob.get(Calendar.MONTH)) {
+        age--;  
+      } else if (today.get(Calendar.MONTH) == dob.get(Calendar.MONTH)
+          && today.get(Calendar.DAY_OF_MONTH) < dob.get(Calendar.DAY_OF_MONTH)) {
+        age--;  
+      }
+      return age;
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+    return -1;
   }
 }
