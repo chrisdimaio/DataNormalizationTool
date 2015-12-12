9de3233f0b57dccb051d2b7159e685f00e75b631
Chris <chrisdimaio@gmail.com>
2015-12-12 18:18:31 -0500

    Renamed.

---
+++ src/main/java/datanormalizationtool/Error.java
@@ -0,0 +1,12 @@
+package datanormalizationtool;
+
+/**
+ * Enum of cell flags. *This might makes sense to be called error.
+ */
+public enum Error {
+  AGE_GRADE_MISMATCH,
+  NO_FLAG,
+  NO_MIDDLE_NAME,
+  UNKNOWN_TOWN,
+  UNKNOWN_GRADE,
+}
