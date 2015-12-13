3780b54cf8aade90da4e952882f6228595718ce5
Chris <chrisdimaio@gmail.com>
2015-12-12 19:40:04 -0500

    Create standardizeDate() tests.

---
+++ src/test/java/datanormaliztiontool/DateHandlerTests.java
@@ -0,0 +1,57 @@
+package datanormaliztiontool;
+
+import static org.junit.Assert.assertEquals;
+
+import datanormalizationtool.DateHandler;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import org.junit.Test;
+
+/**
+ * DateHandler class tests.
+ */
+public class DateHandlerTests {
+  private static final Date EXPECTED_DATE = new GregorianCalendar(2015, 6, 4).getTime();
+  private static final String EXPECTED_DATE_STRING = "Sat Jul 04 00:00:00 EDT 2015";
+  
+  @Test
+  public void standardizeDateTestOne() {
+    assertEquals(EXPECTED_DATE, DateHandler.standardizeDate("07/04/2015"));
+  }
+  
+  @Test
+  public void standardizeDateTestTwo() {
+    assertEquals(EXPECTED_DATE, DateHandler.standardizeDate("07-04-2015"));
+  }
+  
+  @Test
+  public void standardizeDateTestThree() {
+    assertEquals(EXPECTED_DATE, DateHandler.standardizeDate("07/04/15"));
+  }
+  
+  @Test
+  public void standardizeDateTestFour() {
+    assertEquals(EXPECTED_DATE, DateHandler.standardizeDate("07-04-15"));
+  }
+  
+  @Test
+  public void standardizeDateTestFive() {
+    assertEquals(EXPECTED_DATE, DateHandler.standardizeDate("July 4, 2015"));
+  }
+  
+  @Test
+  public void standardizeDateTestSix() {
+    assertEquals(EXPECTED_DATE, DateHandler.standardizeDate("July 4TH, 2015"));
+  }
+  
+  @Test
+  public void standardizeDateTestSeven() {
+    assertEquals(EXPECTED_DATE, DateHandler.standardizeDate("Jul 4th, 2015"));
+  }
+  
+  @Test
+  public void standardizeDateTestEight() {
+    assertEquals(EXPECTED_DATE, DateHandler.standardizeDate("Jul 4, 2015"));
+  }
+}
