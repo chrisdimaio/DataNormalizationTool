6c5634c4312211d1c8936e101c533722f50030ba
Chris DiMaio <chrisdimaio@gmail.com>
2015-12-16 14:10:45 -0500

    First commit.

---
+++ src/main/java/datanormalizationtool/datahandlers/DeseDataHandler.java
@@ -0,0 +1,22 @@
+package datanormalizationtool.datahandlers;
+
+import datanormalizationtool.TableData;
+import java.io.File;
+import org.apache.poi.ss.usermodel.Sheet;
+
+/**
+ *
+ * @author cdimaio
+ */
+public class DeseDataHandler extends TableData {
+  
+  @Override
+  public void loadData(File file, int sheetIndex) {
+    
+  }
+  
+  @Override
+  protected boolean foundHeaderRow(Sheet sheet) {
+    return false;
+  }
+}
