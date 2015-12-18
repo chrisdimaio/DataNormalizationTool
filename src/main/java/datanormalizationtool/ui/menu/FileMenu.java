6da181e0a9e9e5e00ec6a1ae03200cffcc054d60
Chris DiMaio <chrisdimaio@gmail.com>
2015-12-18 16:54:01 -0500

    Added menu items and icons.

--- src/main/java/datanormalizationtool/ui/menu/FileMenu.java
+++ src/main/java/datanormalizationtool/ui/menu/FileMenu.java
@@ -3,15 +3,17 @@ package datanormalizationtool.ui.menu;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JSeparator;
+import javax.swing.UIManager;
 
 /**
  *
  * @author chris
  */
 public class FileMenu extends JMenu {
+  private JMenuItem exit;
+  private JMenuItem newFile;
   private JMenuItem open;
   private JMenuItem save;
-  private JMenuItem exit;
   
   public FileMenu() {
     super("File");
@@ -19,10 +21,13 @@ public class FileMenu extends JMenu {
   }
   
   private void setUpFileMenu(){
-    open = new JMenuItem("Open");
+    newFile = new JMenuItem("New", UIManager.getIcon("FileView.fileIcon"));
+    add(newFile);
+    
+    open = new JMenuItem("Open", UIManager.getIcon("FileView.directoryIcon"));
     add(open);
     
-    save = new JMenuItem("Save");
+    save = new JMenuItem("Save", UIManager.getIcon("FileView.floppyDriveIcon"));
     add(save);
     
     add(new JSeparator());
